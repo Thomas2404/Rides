2ef51aaef962edf803b4c764c1770133694fc1eb
Thomas <Thomas2404@users.noreply.github.com>
2021-02-05 12:15:01 -0800

    Ride vehicle will now automatically spawn when you stay within 5 blocks of the spawn location for 5 seconds. Will cancel the countdown if you leave the area.

--- src/tech/thomas2404/rides/Rides.java
+++ src/tech/thomas2404/rides/Rides.java
@@ -6,11 +6,13 @@ import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Minecart;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.vehicle.VehicleExitEvent;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -24,6 +26,12 @@ public class Rides extends JavaPlugin {
     //Sets up an array so that we can keep certain players from leaving the vehicle while it's moving. Also sets up for the bukkit runnable.
     Plugin plugin = this;
     ArrayList<String> ridingPlayers = new ArrayList<>();
+    ArrayList<String> countingDown = new ArrayList<>();
+    Boolean startCountdown = false;
+    Boolean riding = false;
+    Boolean cancelCountDown = false;
+    int x = 0;
+    int y = 0;
 
     @Override
     public void onEnable() {
@@ -51,6 +59,9 @@ public class Rides extends JavaPlugin {
                 Location startLocation = new Location(Bukkit.getWorld("world"), 100, 4, 100);
                 Minecart vehicle = (Minecart) world.spawnEntity(startLocation, EntityType.MINECART);
                 ridingPlayers.add(player.getName());
+
+
+
                 new BukkitRunnable() {
 
                     //Sets up some math for the ride path.
@@ -70,6 +81,7 @@ public class Rides extends JavaPlugin {
                             this.cancel();
                             ridingPlayers.remove(player.getName());
                             vehicle.remove();
+                            riding = false;
                         }
                     }
                 }.runTaskTimer(plugin, 0, 1);
@@ -85,12 +97,76 @@ public class Rides extends JavaPlugin {
 
     //Sets up the listener so that players cant leave the ride vehicle while riding the ride.
     public class listener implements Listener {
+
         @EventHandler
         public void exitVehicle(VehicleExitEvent event) {
             //If the player is riding and tries to exit the vehicle, cancel the event.
             String player = event.getExited().getName();
             if (ridingPlayers.contains(player)) {
                 event.setCancelled(true);
+            }
+        }
+
+        @EventHandler
+        public void nearRide(PlayerMoveEvent event) {
+
+            Player player = event.getPlayer();
+
+            Location location = new Location(Bukkit.getWorld("world"), 100 , 4, 100);
+
+            double distance = location.distance(player.getLocation());
+
+            if (distance < 5) {
+                if (!startCountdown && !riding) {
+
+                    startCountdown = true;
+                    countingDown.add(player.getName());
+
+                    World world = player.getWorld();
+                    Location screenLocation = new Location (Bukkit.getWorld("world"), 100, 4, 100);
+                    ArmorStand screen = (ArmorStand) world.spawnEntity(screenLocation, EntityType.ARMOR_STAND);
+                    screen.setVisible(false);
+                    screen.setCustomName("The ride will spawn in 5 seconds! Please don't leave!");
+                    screen.setCustomNameVisible(true);
+
+
+                    Long timeInTicks = 20L;
+                    new BukkitRunnable() {
+                        @Override
+                        public void run() {
+
+                            x++;
+                            y--;
+
+                            screen.setCustomName("The ride will spawn in " + y + " seconds! Please don't leave!");
+
+                            if (cancelCountDown) {
+                                startCountdown = false;
+                                countingDown.remove(player.getName());
+                                screen.remove();
+                                cancelCountDown = false;
+                                this.cancel();
+                                x = 0;
+                                y = 5;
+                            }
+                            if (x >= 5) {
+                                player.chat("/ride");
+                                countingDown.remove(player.getName());
+                                startCountdown = false;
+                                riding = true;
+                                screen.remove();
+                                this.cancel();
+                                x = 0;
+                                y = 5;
+                            }
+                        }
+                    }.runTaskTimer(plugin, timeInTicks, timeInTicks);
+                }
+            }
+
+            if ((distance > 5) && countingDown.contains(player.getName())) {
+                cancelCountDown = true;
+                countingDown.remove(player.getName());
             }
         }
     }
